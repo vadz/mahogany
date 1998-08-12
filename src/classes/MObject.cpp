d07df4d9728361ac9d62f867c40d430f459a5dbb
Vadim Zeitlin <vz-mahogany@zeitlins.org>
1998-08-12 22:13:52 +0000

    implementation of MObject class

---
+++ src/classes/MObject.cpp
@@ -0,0 +1,91 @@
+///////////////////////////////////////////////////////////////////////////////
+// Project:     M - cross platform e-mail GUI client
+// File name:   MObject.cpp - implementation of MObject
+// Comment:     the only non inline MObject functions are diagnostic ones
+// Author:      Vadim Zeitlin
+// Modified by: 
+// Created:     09.08.98
+// CVS-ID:      $Id$
+// Copyright:   (c) 1998 Vadim Zeitlin <zeitlin@dptmaths.ens-cachan.fr>
+// Licence:     M license
+///////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+#include "Mpch.h"
+
+#ifdef DEBUG
+
+#include "MObject.h"
+
+#include <wx/log.h>
+#include <wx/dynarray.h>
+
+// ----------------------------------------------------------------------------
+// this module global variables
+// ----------------------------------------------------------------------------
+WX_DEFINE_ARRAY(MObject *, ArrayObjects);
+
+static ArrayObjects gs_aObjects;
+
+// ============================================================================
+// implementation
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// debug only functions
+// ----------------------------------------------------------------------------
+void MObject::CheckLeaks()
+{
+  uint nCount = gs_aObjects.Count();
+
+  if ( nCount > 0 ) {
+    wxFAIL_MSG("MObject memory leaks detected, see debug log for details.");
+
+    wxLogDebug("%d MObjects leaked:", nCount);
+  }
+
+  for ( uint n = 0; n < nCount; n++ ) {
+    wxLogDebug("Object %d: %s", n, gs_aObjects[n]->Dump().c_str());
+  }
+}
+
+String MObject::Dump() const
+{
+  String str;
+  str.Printf("MObject, m_nRef = %d", m_nRef);
+
+  return str;
+}
+
+// ----------------------------------------------------------------------------
+// debug implementations of other functions
+// ----------------------------------------------------------------------------
+
+MObject::MObject()
+{ 
+  gs_aObjects.Add(this);
+
+  m_nRef = 1;
+}
+
+bool MObject::Unlock()
+{ 
+  wxASSERT(m_nRef > 0);
+
+  if ( !--m_nRef ) {
+    gs_aObjects.Remove(this);
+    delete this;
+
+    return FALSE;
+  }
+  
+  return TRUE;
+}
+
+#endif //DEBUG
