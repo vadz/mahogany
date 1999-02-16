d2d049d6c72b922a0d7e2f6cc65a52d3ae3f3ede
Karsten Ballüder <karsten@ballueder.net>
1999-02-16 17:41:17 +0000

    gettext_noop() macro was misdefined

--- include/Mconfig.h
+++ include/Mconfig.h
@@ -217,7 +217,7 @@
 // macro which marks strings for later extraction with xgettext(1): it
 // intentionally does nothing else!
 #ifndef gettext_noop
-   #define gettext_noop(s)    (s)
+   #define gettext_noop(s)    s
 #endif // gettext_noop
 
 #endif
