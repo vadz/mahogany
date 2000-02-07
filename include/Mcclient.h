4ddf9138bc4acc3931277d2361c85782ddce26a0
Karsten Ballüder <karsten@ballueder.net>
2000-02-07 10:03:11 +0000

    temp fix

---
+++ include/Mcclient.h
@@ -0,0 +1,13 @@
+/// forgot to check in, temporary fix
+#define private ccprivate
+extern "C"
+{
+#     include <stdio.h>
+#     include <mail.h>
+#     include <osdep.h>
+#     include <rfc822.h>
+#     include <smtp.h>
+#     include <nntp.h>
+#     include <misc.h>
+}
+undef private
