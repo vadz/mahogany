aafd8e4c509514f20b2f8483eecc9bc3d8f1c5bb
Daniel Seifert <dseifert@gmx@de>
2000-02-24 20:51:08 +0000

    Removed potential bug.

--- src/adb/ProvPalm.cpp
+++ src/adb/ProvPalm.cpp
@@ -1,22 +1,14 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Project:     M - cross platform e-mail GUI client
 // File name:   adb/ProvPalm.cpp - Palm based AdbDataProvider
-// Purpose:     simple implementations of AdbBook and AdbDataProvider
+// Purpose:     implementation of PalmAdbBook and PalmAdbDataProvider
 // Author:      Daniel Seifert
 // Modified by:
 // Created:     15.02.00
-// CVS-ID:      
+// CVS-ID:      $Id:
 // Copyright:   (c) 2000 Daniel Seifert <dseifert@gmx.de>
 // Licence:     M license
 ///////////////////////////////////////////////////////////////////////////////
-
-/*
-  This is the AdbDataProvider for the PalmOS Addressbook. 
-*/
-
-// ============================================================================
-// declarations
-// ============================================================================
 
 // ----------------------------------------------------------------------------
 // headers
@@ -36,16 +28,6 @@
 #  include <ctype.h>
 #endif //USE_PCH
 
-// wxWindows
-#include <wx/string.h>
-#include <wx/log.h>
-#include <wx/intl.h>
-#include <wx/dynarray.h>
-#include <wx/file.h>
-#include <wx/textfile.h>
-#include <wx/confbase.h>
-#include <wx/fileconf.h>
-
 #include "adb/AdbManager.h"
 #include "adb/AdbEntry.h"
 #include "adb/AdbBook.h"
@@ -54,6 +36,7 @@
 #include <pi-address.h>
 #include "MModule.h"
 #include "kbList.h"
+#include <adb/ProvPalm.h>   // class declarations
 
 // ----------------------------------------------------------------------------
 // constants
@@ -62,183 +45,7 @@
 #define   ADB_HEADER        "ADB_Header"
 #define   ADB_HEADER_NAME   "Name"
 #define   ADB_HEADER_DESC   "Description"
-
 #define   ADB_ENTRIES       "ADB_Entries"
-
-// ----------------------------------------------------------------------------
-// private classes
-// ----------------------------------------------------------------------------
-
-// fwd decl
-class PalmEntry;
-class PalmEntryGroup;
-class PalmBook;
-class PalmDataProvider;
-
-class PalmOSModule : public MModule
-{
-public:
-   void Synchronise(PalmBook *p_Book);
-};
-
-// our AdbEntryData implementation
-class PalmEntry : public AdbEntryStoredInMemory
-{
-public:
-  // ctor
-  PalmEntry(PalmEntryGroup *pGroup, const String& strName, bool bNew = FALSE);
-  virtual ~PalmEntry(void);
-
-  // implement interface methods
-    // AdbEntry
-  virtual AdbEntryGroup *GetGroup() const;
-
-  void Load(struct Address a);
-  bool Save();
-
-  // an easier to use GetName()
-  const char *GetName() const { return m_astrFields[0]; }
-
-  // if it's not, we will be deleted, so it really must be something fatal
-  bool IsOk() const { return m_pGroup != NULL; }
-
-  MOBJECT_DEBUG(PalmEntry)
-
-private:
-  PalmEntryGroup *m_pGroup;     // the group which contains us (never NULL)
-
-  GCC_DTOR_WARN_OFF
-};
-
-KBLIST_DEFINE(PalmEntryList, PalmEntry);
-
-// our AdbEntryGroup implementation
-class PalmEntryGroup : public AdbEntryGroup
-{
-public:
-  // ctors
-    // the normal one
-  PalmEntryGroup(PalmEntryGroup *pParent, const wxString& strName,
-               bool bNew = FALSE);
-    // this one is only used for the root group
-  PalmEntryGroup(void);
-
-  // implement interface methods
-    // AdbEntryGroup
-  virtual AdbEntryGroup *GetGroup() const { return m_pParent; }
-  virtual String GetName() const { return m_strName; }
-
-  virtual size_t GetEntryNames(wxArrayString& aNames) const;
-  virtual size_t GetGroupNames(wxArrayString& aNames) const;
-
-  virtual AdbEntry *GetEntry(const String& name) const;
-  virtual AdbEntryGroup *GetGroup(const String& name) const;
-
-  virtual bool Exists(const String& path) const;
-
-  virtual AdbEntry *CreateEntry(const String& strName);
-  virtual void AddEntry(PalmEntry* p_Entry);
-  virtual AdbEntryGroup *CreateGroup(const String& strName);
-
-  virtual void DeleteEntry(const String& strName);
-  virtual void DeleteGroup(const String& strName);
-
-  virtual AdbEntry *FindEntry(const char *szName);
-
-  MOBJECT_DEBUG(PalmEntry)
-
-private:
-  virtual ~PalmEntryGroup();
-
-  wxString        m_strName;      // our name
-  PalmEntryGroup *m_pParent;      // the parent group (never NULL)
-  PalmEntryList  *m_entries;      // a list of all entries
-  GCC_DTOR_WARN_OFF
-};
-
-// our AdbBook implementation
-class PalmBook : public AdbBook
-{
-public:
-  PalmBook(const String& filename);
-
-  // implement interface methods
-    // AdbElement
-  virtual AdbEntryGroup *GetGroup() const { return m_pRootGroup; }
-
-    // AdbEntryGroup
-  virtual AdbEntry *GetEntry(const String& name) const
-    { return m_pRootGroup->GetEntry(name); }
-
-  virtual bool Exists(const String& path) const
-    { return m_pRootGroup->Exists(path); }
-
-  virtual size_t GetEntryNames(wxArrayString& aNames) const
-    { return m_pRootGroup->GetEntryNames(aNames); }
-  virtual size_t GetGroupNames(wxArrayString& aNames) const
-    { return m_pRootGroup->GetGroupNames(aNames); }
-
-  virtual AdbEntryGroup *GetGroup(const String& name) const
-    { return m_pRootGroup->GetGroup(name);}
-
-  virtual AdbEntry *CreateEntry(const String& strName)
-    { return m_pRootGroup->CreateEntry(strName); }
-  virtual AdbEntryGroup *CreateGroup(const String& strName)
-    { return m_pRootGroup->CreateGroup(strName); }
-
-  virtual void DeleteEntry(const String& strName)
-    { m_pRootGroup->DeleteEntry(strName); }
-  virtual void DeleteGroup(const String& strName)
-    { m_pRootGroup->DeleteGroup(strName); }
-
-  virtual AdbEntry *FindEntry(const char *szName)
-    { return m_pRootGroup->FindEntry(szName); }
-
-  // there can be only one PalmADB
-  virtual bool IsSameAs(const String& name) const
-    { return TRUE; }
-  virtual String GetName() const;
-
-  virtual void SetUserName(const String& name);
-  virtual String GetUserName() const;
-
-  virtual void SetDescription(const String& desc);
-  virtual String GetDescription() const;
-
-  virtual size_t GetNumberOfEntries() const;
-
-  virtual bool IsLocal() const { return TRUE; }
-  virtual bool IsReadOnly() const;
-
-  virtual bool Flush();
-
-  MOBJECT_DEBUG(PalmBook)
-
-private:
-  virtual ~PalmBook();
-
-  wxString m_strName,
-           m_strUserName,
-           m_strDescription;
-           
-  PalmEntryGroup *m_pRootGroup; // the ADB_Entries group
-  GCC_DTOR_WARN_OFF
-};
-
-// our AdbDataProvider implementation
-class PalmDataProvider : public AdbDataProvider
-{
-public:
-  // implement interface methods
-  virtual AdbBook *CreateBook(const String& name);
-  virtual bool EnumBooks(wxArrayString& aNames);
-  virtual bool DeleteBook(AdbBook *book);
-  virtual bool TestBookAccess(const String& name, AdbTests test);
-
-  MOBJECT_DEBUG(PalmDataProvider)
-
-  DECLARE_ADB_PROVIDER(PalmDataProvider);
-};
 
 IMPLEMENT_ADB_PROVIDER(PalmDataProvider, TRUE, "PalmOS-ADB (Broken! Do not use!)", Name_No);
 
@@ -293,6 +100,7 @@ void PalmEntry::Load(struct Address a)
   SetField(AdbField_H_Fax, a.entry[5]);
   SetField(AdbField_H_Street, a.entry[8]);
   SetField(AdbField_H_City, a.entry[9]);
+
   // Locality == state?
   SetField(AdbField_H_Locality, a.entry[10]);
   SetField(AdbField_H_Postcode, a.entry[11]);
@@ -313,8 +121,6 @@ void PalmEntry::Load(struct Address a)
 // save entry to wxFileConfig (doesn't check if it's modified or not)
 bool PalmEntry::Save()
 {
-  wxCHECK_MSG( m_bDirty, TRUE, "shouldn't save unmodified PalmEntry" );
-
   // TODO: save changed entries to Palm
   return FALSE;
 }
@@ -339,9 +145,8 @@ PalmEntryGroup::PalmEntryGroup(PalmEntryGroup *pParent,
   m_strName = strName;
   m_entries = new PalmEntryList(false);
 
-  // FIXME: Have to check whether m_entries could be created
-     
   if ( bNew ) {
+    // ??
     // force creation of the group
   }
 }
