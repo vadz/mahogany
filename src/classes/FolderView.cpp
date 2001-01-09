f78c25ff6e5a796cd2f300ad15afbc62dc4d685e
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2001-01-09 13:34:10 +0000

    file I forgot to add

---
+++ src/classes/FolderView.cpp
@@ -0,0 +1,111 @@
+// -*- c++ -*-/////////////////////////////////////////////////////////////////
+// Project:     M - cross platform e-mail GUI client
+// File name:   FolderView.cpp - implementation of FolderView base methods
+// Author:      Vadim Zeitlin
+// Modified by:
+// Created:     08.01.01
+// CVS-ID:      $Id$
+// Copyright:   (c) 2001 Vadim Zeitlin <zeitlin@dptmaths.ens-cachan.fr>
+// Licence:     M license
+///////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+
+#include "Mpch.h"
+
+#ifndef  USE_PCH
+#   include "Mcommon.h"
+#   include "Mdefaults.h"
+#   include "MApplication.h"
+#   include "gui/wxMainFrame.h"
+#   include "FolderView.h"
+#endif // USE_PCH
+
+// ============================================================================
+// implementation
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// FolderView creation
+// ----------------------------------------------------------------------------
+
+FolderView::FolderView()
+{
+    if ( !MEventManager::RegisterAll
+          (
+            *this,
+            MEventId_FolderTreeChange,  &m_regCookieTreeChange,
+            MEventId_FolderUpdate,      &m_regCookieFolderUpdate,
+            MEventId_MsgStatus,         &m_regCookieMsgStatus,
+            MEventId_ASFolderResult,    &m_regCookieASFolderResult,
+            MEventId_AppExit,           &m_regCookieAppExit,
+            MEventId_Null
+          ) )
+    {
+        FAIL_MSG( "Failed to register folder view with event manager" );
+    }
+}
+
+void FolderView::DeregisterEvents(void)
+{
+    MEventManager::DeregisterAll(&m_regCookieTreeChange,
+                                 &m_regCookieFolderUpdate,
+                                 &m_regCookieMsgStatus,
+                                 &m_regCookieASFolderResult,
+                                 &m_regCookieAppExit,
+                                 NULL);
+}
+
+FolderView::~FolderView()
+{
+}
+
+// ----------------------------------------------------------------------------
+// event dispatching
+// ----------------------------------------------------------------------------
+
+bool FolderView::OnMEvent(MEventData& ev)
+{
+    if ( ev.GetId() == MEventId_FolderTreeChange )
+    {
+        MEventFolderTreeChangeData& event = (MEventFolderTreeChangeData &)ev;
+        if ( event.GetChangeKind() == MEventFolderTreeChangeData::Delete )
+            OnFolderDeleteEvent(event.GetFolderFullName());
+    }
+    else if ( ev.GetId() == MEventId_ASFolderResult )
+        OnASFolderResultEvent((MEventASFolderResultData &) ev );
+    else if ( ev.GetId() == MEventId_MsgStatus )
+        OnMsgStatusEvent((MEventMsgStatusData&)ev );
+    else if ( ev.GetId() == MEventId_FolderUpdate )
+        OnFolderUpdateEvent((MEventFolderUpdateData&)ev );
+    else if ( ev.GetId() == MEventId_AppExit )
+        OnAppExit();
+
+    return true; // continue evaluating this event
+}
+
+void FolderView::OnAppExit()
+{
+   // do we want to reopen this folder the next time automatically?
+   if ( READ_APPCONFIG(MP_REOPENLASTFOLDER) )
+   {
+      // don't remember the folder opened in the main frame - the main frame
+      // does it itself
+      if ( m_folderName != ((wxMainFrame *)mApplication->TopLevelFrame())
+                             ->GetFolderName() )
+      {
+         String foldersToReopen = READ_APPCONFIG(MP_OPENFOLDERS);
+         if ( !foldersToReopen.empty() )
+            foldersToReopen += ';';
+         foldersToReopen += m_folderName;
+
+         mApplication->GetProfile()->writeEntry(MP_OPENFOLDERS, foldersToReopen);
+      }
+   }
+}
