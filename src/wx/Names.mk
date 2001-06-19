ae17191d284dce1552ea0dc6daaba42bbd14a26b
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2001-06-19 18:50:24 +0000

    fix for vcc.h not found error

--- src/wx/Names.mk
+++ src/wx/Names.mk
@@ -2,3 +2,7 @@
 # $Id$
 
 include $(wildcard .src/wx/*/Names.mk) $(wildcard $(patsubst .src/%/Names.mk,%/*.d,$(wildcard .src/wx/*/Names.mk)))
+
+# need to explicitly specify the path containing vcc.h when compiling in a
+# separate build dir
+CFLAGS_wx_vcard_vcc_o := -I.src/wx/vcard
