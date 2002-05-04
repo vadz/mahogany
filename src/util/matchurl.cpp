210e634453f3fa3b767f4305145be80de739fea2
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2002-05-04 11:46:32 +0000

    fixed a signed/unsigned warning and reformatted the code

--- src/util/matchurl.cpp
+++ src/util/matchurl.cpp
@@ -29,94 +29,91 @@
 // private classes
 // ----------------------------------------------------------------------------
 
-// TODO: translate Xavier's comments and variable names to English
-
 /// KeywordDetectorCell represents a single keyword letter with links from it
-class KeywordDetectorCell {
-
-  friend class KeywordDetector;
-
+class KeywordDetectorCell
+{
 public:
-  KeywordDetectorCell(char c) :
+   KeywordDetectorCell(char c) :
       _c(c), _son(NULL), _brother(NULL), _isKey(0), _back(NULL) {}
 
-   ~KeywordDetectorCell() {  
-      if (_c != '\000') {
+   ~KeywordDetectorCell()
+   {
+      if (_c != '\000')
+      {
          delete _son;
          delete _brother;
       } // else: this is a back-node, and thus it points up in the
-        // tree, to a node that is already deleted, or will soon be.
+      // tree, to a node that is already deleted, or will soon be.
    }
 
 private:
-  char _c;
-  KeywordDetectorCell* _son;
-  KeywordDetectorCell* _brother;
-  int _isKey;
-  KeywordDetectorCell* _back;
+   char _c;
+   KeywordDetectorCell* _son;
+   KeywordDetectorCell* _brother;
+   int _isKey;
+   KeywordDetectorCell* _back;
 
-  KeywordDetectorCell* computeBackArc(KeywordDetectorCell* root,
-                                      KeywordDetectorCell* parentBack);
-  void computeBackArcs(KeywordDetectorCell* root,
-                       KeywordDetectorCell* parentBack);
+   KeywordDetectorCell* computeBackArc(KeywordDetectorCell* root,
+         KeywordDetectorCell* parentBack);
+   void computeBackArcs(KeywordDetectorCell* root,
+         KeywordDetectorCell* parentBack);
+
+   friend class KeywordDetector;
 };
 
 
 /**
   * Keywords scanner
   */
-class KeywordDetector {
+class KeywordDetector
+{
 public:
-  KeywordDetector() : _root(NULL) { }
-  ~KeywordDetector() { 
-     delete _root; 
-  }
+   KeywordDetector() : _root(NULL) { }
+   ~KeywordDetector() { delete _root; }
 
 public:
+   /// Adds a new keyword to the list of detected keywords
+   void addNewKeyword(const char* key);
 
-  /// Adds a new keyword to the list of detected keywords
-  void addNewKeyword(const char* key);
+   /** Returns the length of the longest keyword
+     starting at the beginning of the string given as parameter.
+     Returns 0 if no keyword found or not at the beginning of
+     the string.
+     This method does not need that computeBackArcs has been called.
+     Keywords can be added between calls to this method.
+    */
+   int scanAtStart(const char* toBeScanned);
 
-  /** Returns the length of the longest keyword
-      starting at the beginning of the string given as parameter.
-      Returns 0 if no keyword found or not at the beginning of
-      the string.
-      This method does not need that computeBackArcs has been called.
-      Keywords can be added between calls to this method.
-      */
-  int scanAtStart(const char* toBeScanned);
+   /** Scans the given string to find a keyword.
+     Returns the starting position of the first
+     keyword in the string.
+     lng is the length of the longest keyword that
+     starts in this position.
+     computeBackArcs must have been called before the
+     first call to this function.
+    */
+   int scan(const char* toBeScanned, int& lng) {
+      return scan(toBeScanned, lng, _root);
+   }
 
-  /** Scans the given string to find a keyword.
-      Returns the starting position of the first
-      keyword in the string.
-      lng is the length of the longest keyword that
-      starts in this position.
-      computeBackArcs must have been called before the
-      first call to this function.
-      */
-  int scan(const char* toBeScanned, int& lng) {
-    return scan(toBeScanned, lng, _root);
-  }
-
-  /** Does all the precomputations needed after all the
-      keywords have been added, and before the first call
-      to scan.
-      */
-  void computeBackArcs() {
-    _root->computeBackArcs(_root, 0);
-  }
-
+   /** Does all the precomputations needed after all the
+     keywords have been added, and before the first call
+     to scan.
+    */
+   void computeBackArcs() {
+      _root->computeBackArcs(_root, 0);
+   }
 
 private:
-  void addNewKeyword(const char* key,
-                     KeywordDetectorCell* current,
-                     int toBeAdded = 1);
-  int scanAtStart(const char* toBeScanned,
-                  KeywordDetectorCell* current,
-                  int longueurDejaVue, int lngDernierTrouve);
-  int scan(const char* toBeScanned, int& lng, KeywordDetectorCell* current);
+   void addNewKeyword(const char* key,
+         KeywordDetectorCell* current,
+         int toBeAdded = 1);
+   int scanAtStart(const char* toBeScanned,
+         KeywordDetectorCell* current,
+         int longueurDejaVue, int lngDernierTrouve);
+   int scan(const char* toBeScanned, int& lng, KeywordDetectorCell* current);
 
-  KeywordDetectorCell* _root;
+   KeywordDetectorCell* _root;
 };
 
 /// URLDetector simply uses KeywordDetector to detect specifically the URLs
@@ -139,213 +136,274 @@ public:
 // KeywordDetector implementation
 // ============================================================================
 
-void KeywordDetector::addNewKeyword(const char* key) {
-  //
-  // Ajout d'un mot-clef dans l'arbre
-  //
-  if (key && (strlen(key) > 0))
-    addNewKeyword(key, _root);
+void KeywordDetector::addNewKeyword(const char* key)
+{
+   //
+   // Ajout d'un mot-clef dans l'arbre
+   //
+   if (key && (strlen(key) > 0))
+      addNewKeyword(key, _root);
 }
 
 
 void KeywordDetector::addNewKeyword(const char* key,
                                     KeywordDetectorCell* current,
-                                    int toBeAdded) {
-  if (! current) {
-    // This is the first keyword inserted
-    // We must create adn save the root of the tree
-    KeywordDetectorCell* newCell = new KeywordDetectorCell(key[0]);
-    _root = newCell;
-    addNewKeyword(key, _root);
-    return;
-  }
-  if (current->_c == key[0]) {
-    // The key is correct: this is the correct cell
-    if (key[1] == '\000') {
-      // And we have reached the end of a keyword
-      // Donc la cellule courante marque la fin
-      // d'un mot-clef
-      current->_isKey = toBeAdded;
-    } else {
-      // The keyword to add (or remove) is not finished
-      if (! current->_son) {
-        // No son yet. We have to create one
-        KeywordDetectorCell* newCell = new KeywordDetectorCell(key[1]);
-        current->_son = newCell;
-      }
-      addNewKeyword(key+1, current->_son);
-    }
-  } else {
-    // This is not the correct cell
-    if (! current->_brother) {
-      // No brother. Let's create one
+                                    int toBeAdded)
+{
+   if (! current)
+   {
+      // This is the first keyword inserted
+      // We must create adn save the root of the tree
       KeywordDetectorCell* newCell = new KeywordDetectorCell(key[0]);
-      current->_brother = newCell;
-    }
-    addNewKeyword(key, current->_brother);
-  }
+      _root = newCell;
+      addNewKeyword(key, _root);
+      return;
+   }
+
+   if (current->_c == key[0])
+   {
+      // The key is correct: this is the correct cell
+      if (key[1] == '\000')
+      {
+         // And we have reached the end of a keyword
+         // Donc la cellule courante marque la fin
+         // d'un mot-clef
+         current->_isKey = toBeAdded;
+      }
+      else
+      {
+         // The keyword to add (or remove) is not finished
+         if (! current->_son) {
+            // No son yet. We have to create one
+            KeywordDetectorCell* newCell = new KeywordDetectorCell(key[1]);
+            current->_son = newCell;
+         }
+         addNewKeyword(key+1, current->_son);
+      }
+   }
+   else
+   {
+      // This is not the correct cell
+      if (! current->_brother)
+      {
+         // No brother. Let's create one
+         KeywordDetectorCell* newCell = new KeywordDetectorCell(key[0]);
+         current->_brother = newCell;
+      }
+
+      addNewKeyword(key, current->_brother);
+   }
 }
 
 
-int KeywordDetector::scanAtStart(const char* toBeScanned, KeywordDetectorCell* current,
-                                 int longueurDejaVue, int lngDernierTrouve) {
-  if (! current) {
-    return lngDernierTrouve;
-  }
-  if (current->_c == toBeScanned[0]) {
-    // This is the correct cell
-    if (current->_isKey == 1) {
-      // And this is the end of a keyword
-      // let's try to find a longer one, but do
-      // remember that we already saw one, the length
-      // of which is longueurDejaVue+1
-      return scanAtStart(toBeScanned+1,
-                         current->_son,
-                         longueurDejaVue+1,
-                         longueurDejaVue+1);
-    } else {
-      // This is not the end of a keyword
-      // Go on
-      assert(current->_son);
-      return scanAtStart(toBeScanned+1, current->_son, longueurDejaVue+1, lngDernierTrouve);
-    }
-  } else {
-    // Not a correct cell
-    if (! current->_brother) {
-      // And no more cell. Let's return the length
-      // we may have already found
+int
+KeywordDetector::scanAtStart(const char* toBeScanned,
+                             KeywordDetectorCell* current,
+                             int longueurDejaVue,
+                             int lngDernierTrouve)
+{
+   if (! current)
       return lngDernierTrouve;
-    } else {
-      // There is another cell to try
-      return scanAtStart(toBeScanned, current->_brother, longueurDejaVue, lngDernierTrouve);
-    }
-  }
-}
 
-
-
-
-int KeywordDetector::scanAtStart(const char* toBeScanned) {
-  return scanAtStart(toBeScanned, _root, 0, 0);
-}
-
-
-int KeywordDetector::scan(const char* toBeScanned, int& lng, KeywordDetectorCell* current) {
-  int currentPosition = 0;
-  lng = 0;
-  bool atRootLevel = true;
-  while (toBeScanned[0]) {
-    if (current->_c == toBeScanned[0]) {
+   if (current->_c == toBeScanned[0])
+   {
       // This is the correct cell
-      if (current->_isKey == 1) {
-        // And the end of a keyword.
-        // let's try to find a longer one, but do
-        // remember that we already saw one, the length
-        // of which is longueurDejaVue+1
-
-        lng =  scanAtStart(toBeScanned+1,
-                           current->_son,
-                           lng+1,
-                           lng+1);
-        return currentPosition;
-      } else {
-        // This is not the end of a keyword
-        // Go on
-        assert(current->_son);
-        current = current->_son;
-        atRootLevel = false;
-        lng++;
-        toBeScanned = toBeScanned + 1;
+      if (current->_isKey == 1)
+      {
+         // And this is the end of a keyword
+         // let's try to find a longer one, but do
+         // remember that we already saw one, the length
+         // of which is longueurDejaVue+1
+         return scanAtStart(toBeScanned+1,
+               current->_son,
+               longueurDejaVue+1,
+               longueurDejaVue+1);
       }
-    } else {
+      else
+      {
+         // This is not the end of a keyword
+         // Go on
+         CHECK( current->_son, -1, "current cell must have a son" );
+         return scanAtStart(toBeScanned+1, current->_son, longueurDejaVue+1, lngDernierTrouve);
+      }
+   }
+   else
+   {
       // Not a correct cell
-      current = current->_brother;
-      if (!current)  {
-        current = _root;
-        if (atRootLevel) {
-          toBeScanned++;
-          if ( !lng )
-              lng = 1;
-        }
-        atRootLevel = true;
-        currentPosition = currentPosition + lng;
-        lng = 0;
-      } else if (current->_c == '\000') {
-        // This is a 'back-link'
-        currentPosition = currentPosition + lng - current->_isKey;
-        lng = current->_isKey;
-        assert (lng > 0);
-        current = current->_son;
+      if (! current->_brother)
+      {
+         // And no more cell. Let's return the length
+         // we may have already found
+         return lngDernierTrouve;
       }
-    }
-  }
-  lng = 0;
-  return 0;
+      else
+      {
+         // There is another cell to try
+         return scanAtStart(toBeScanned, current->_brother, longueurDejaVue, lngDernierTrouve);
+      }
+   }
 }
 
-KeywordDetectorCell* KeywordDetectorCell::computeBackArc(KeywordDetectorCell* root,
-                                                         KeywordDetectorCell* parentBack) {
-  assert(parentBack == 0 || parentBack->_c == '\000');
-  KeywordDetectorCell* back = 0;
-  KeywordDetectorCell* current = 0;
-  int backLevel = 0;
-  if (! parentBack) {
-    // Parent node has no back arc. So we look for
-    // nextLetter in root and its siblings
-    current = root;
-    backLevel = 1;
-  } else {
-    // Parent has a back node. So we look for nextLetter
-    // in *the sons* of this back node
-    current = parentBack->_son;
-    backLevel = parentBack->_isKey + 1;
-  }
-  while (current) {
-    if (current->_c == _c) {
-      back = current;
-      break;
-    }
-    current = current->_brother;
-  }
-  if (current) {
-    // We have found our back node
-    // Find our last son
-    KeywordDetectorCell* lastSon = _son;
-    // There must be a son, otherwise the current node would be a keyword
-    // and there is no need for a back node
-    assert(lastSon);
-    while (lastSon->_brother) {
-      lastSon = lastSon->_brother;
-    }
-    assert(! lastSon->_brother);
-    // A cell with key '\000' is points to the back node
-    KeywordDetectorCell* newCell = new KeywordDetectorCell('\000');
-    newCell->_son = current->_son;
-    newCell->_isKey = backLevel;
-    lastSon->_brother = newCell;
-    return newCell;
-  }
-  return 0;
+
+int KeywordDetector::scanAtStart(const char* toBeScanned)
+{
+   return scanAtStart(toBeScanned, _root, 0, 0);
 }
 
-void KeywordDetectorCell::computeBackArcs(KeywordDetectorCell* root,
-                                          KeywordDetectorCell* parentBack) {
-  KeywordDetectorCell* parent = this;
-  bool onRootLevel = (parent == root ? true : false);
-  while (parent) {
-    if (parent->_c == '\000') {
-      break;
-    }
-    KeywordDetectorCell* back = 0;
-    if (! (onRootLevel || parent->_isKey)) {
-      back = parent->computeBackArc(root, parentBack);
-    }
-    KeywordDetectorCell* son = parent->_son;
-    if (son) {
-      son->computeBackArcs(root, back);
-    }
-    parent = parent->_brother;
-  }
+
+int KeywordDetector::scan(const char* toBeScanned,
+                          int& lng, KeywordDetectorCell* current)
+{
+   int currentPosition = 0;
+   lng = 0;
+   bool atRootLevel = true;
+   while (toBeScanned[0])
+   {
+      if (current->_c == toBeScanned[0])
+      {
+         // This is the correct cell
+         if (current->_isKey == 1)
+         {
+            // And the end of a keyword.
+            // let's try to find a longer one, but do
+            // remember that we already saw one, the length
+            // of which is longueurDejaVue+1
+
+            lng =  scanAtStart(toBeScanned+1,
+                  current->_son,
+                  lng+1,
+                  lng+1);
+            return currentPosition;
+         }
+         else
+         {
+            // This is not the end of a keyword
+            // Go on
+            CHECK( current->_son, -1, "current cell must have a son" );
+            current = current->_son;
+            atRootLevel = false;
+            lng++;
+            toBeScanned = toBeScanned + 1;
+         }
+      }
+      else
+      {
+         // Not a correct cell
+         current = current->_brother;
+         if (!current)
+         {
+            current = _root;
+            if (atRootLevel)
+            {
+               toBeScanned++;
+               if ( !lng )
+                  lng = 1;
+            }
+            atRootLevel = true;
+            currentPosition = currentPosition + lng;
+            lng = 0;
+         }
+         else if (current->_c == '\000')
+         {
+            // This is a 'back-link'
+            currentPosition = currentPosition + lng - current->_isKey;
+            lng = current->_isKey;
+            ASSERT_MSG( lng > 0, "length must be non 0" );
+            current = current->_son;
+         }
+      }
+   }
+   lng = 0;
+   return 0;
+}
+
+KeywordDetectorCell *
+KeywordDetectorCell::computeBackArc(KeywordDetectorCell* root,
+                                    KeywordDetectorCell* parentBack)
+{
+   ASSERT_MSG(parentBack == 0 || parentBack->_c == '\000',
+              "logic error in KeywordDetectorCell?");
+
+   KeywordDetectorCell* back = 0;
+   KeywordDetectorCell* current = 0;
+   int backLevel = 0;
+   if (! parentBack)
+   {
+      // Parent node has no back arc. So we look for
+      // nextLetter in root and its siblings
+      current = root;
+      backLevel = 1;
+   }
+   else
+   {
+      // Parent has a back node. So we look for nextLetter
+      // in *the sons* of this back node
+      current = parentBack->_son;
+      backLevel = parentBack->_isKey + 1;
+   }
+
+   while (current)
+   {
+      if (current->_c == _c)
+      {
+         back = current;
+         break;
+      }
+
+      current = current->_brother;
+   }
+
+   if (current)
+   {
+      // We have found our back node
+      // Find our last son
+      KeywordDetectorCell* lastSon = _son;
+
+      // There must be a son, otherwise the current node would be a keyword
+      // and there is no need for a back node
+      CHECK(lastSon, 0, "node with a back link must have a son");
+
+      while (lastSon->_brother)
+      {
+         lastSon = lastSon->_brother;
+      }
+
+      // A cell with key '\000' is points to the back node
+      KeywordDetectorCell* newCell = new KeywordDetectorCell('\000');
+      newCell->_son = current->_son;
+      newCell->_isKey = backLevel;
+      lastSon->_brother = newCell;
+      return newCell;
+   }
+
+   return 0;
+}
+
+void
+KeywordDetectorCell::computeBackArcs(KeywordDetectorCell* root,
+                                     KeywordDetectorCell* parentBack)
+{
+   KeywordDetectorCell* parent = this;
+   bool onRootLevel = (parent == root ? true : false);
+   while (parent)
+   {
+      if (parent->_c == '\000')
+         break;
+
+      KeywordDetectorCell* back = 0;
+      if (! (onRootLevel || parent->_isKey))
+      {
+         back = parent->computeBackArc(root, parentBack);
+      }
+
+      KeywordDetectorCell* son = parent->_son;
+      if (son)
+      {
+         son->computeBackArcs(root, back);
+      }
+
+      parent = parent->_brother;
+   }
 }
 
 // ============================================================================
@@ -364,14 +422,14 @@ static inline bool IsAlnum(char c)
 /// checks a character to be a 'mark' as from RFC2396
 inline bool IsURLMark(char c)
 {
-   return c == '-' || c == '_' || c == '.' || c == '!' || c == '~' || 
+   return c == '-' || c == '_' || c == '.' || c == '!' || c == '~' ||
           c == '*' || c == '\'' || c == '(' || c == ')';
 }
 
 /// checks a character to be 'reserved' as from RFC2396
 inline bool IsURLReserved(char c)
 {
-   return c == ';' || c == '/' || c == '?' || c == ':' || c == '@' || 
+   return c == ';' || c == '/' || c == '?' || c == ':' || c == '@' ||
           c == '&' || c == '=' || c == '+' || c == '$' || c == ',';
 }
 
@@ -498,7 +556,7 @@ match:
          // even with all the checks below we still get too many false
          // positives so consider that only "long" URLs are wrapped where long
          // URLs are defined as the ones containing the CGI script parameters
-         if ( strcspn(start + len, "?&\r") == p - start - len )
+         if ( strcspn(start + len, "?&\r") == (size_t)(p - start - len) )
          {
             // no CGI parameters, suppose it can't wrap
             break;
