e38210d29a476e4498b7a14caa6ba1f69e8bbdb6
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2002-12-05 20:47:36 +0000

    added trivial markup filter: handles *bold* and _italic_ notations

---
+++ src/modules/viewflt/TextMarkup.cpp
@@ -0,0 +1,156 @@
+///////////////////////////////////////////////////////////////////////////////
+// Project:     M - cross platform e-mail GUI client
+// File name:   TextMarkupFilter.cpp: trivial text markup viewer filter
+// Purpose:     trivial text markup is the USENET tradition of using *bold*
+//              and _italic_ markup in the text
+// Author:      Vadim Zeitlin
+// Modified by:
+// Created:     05.12.02
+// CVS-ID:      $Id$
+// Copyright:   (c) 2002 Vadim Zeitlin <vadim@wxwindows.org>
+// Licence:     M license
+///////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+
+#include "Mpch.h"
+
+#ifndef USE_PCH
+   #include "Mcommon.h"
+#endif //USE_PCH
+
+#include "ViewFilter.h"
+
+#include "MTextStyle.h"
+
+// ----------------------------------------------------------------------------
+// TextMarkupFilter declaration
+// ----------------------------------------------------------------------------
+
+class TextMarkupFilter : public ViewFilter
+{
+public:
+   TextMarkupFilter(MessageView *msgView, ViewFilter *next, bool enable)
+      : ViewFilter(msgView, next, enable) { }
+
+protected:
+   virtual void DoProcess(String& text,
+                          MessageViewer *viewer,
+                          MTextStyle& style);
+};
+
+// ============================================================================
+// implementation
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// TextMarkupFilter
+// ----------------------------------------------------------------------------
+
+// this filter has a low priority, it should be applied after all the
+// others to avoid interfering with the URLs -- but it should be applied before
+// Rot13 filter
+IMPLEMENT_VIEWER_FILTER(TextMarkupFilter,
+                        ViewFilter::Priority_Low + 10,
+                        true,      // enabled by default
+                        _("Trivial Markup"),
+                        "(c) 2002 Vadim Zeitlin <vadim@wxwindows.org>");
+
+void
+TextMarkupFilter::DoProcess(String& text,
+                            MessageViewer *viewer,
+                            MTextStyle& style)
+{
+   enum
+   {
+      Normal,
+      Bold,
+      Italic
+   } state = Normal;
+
+   // the '*' or '_' last encountered
+   wxChar chLastSpecial = _T('\0'); // unneeded but silence compiler warning
+   String textNormal,
+          textSpecial;
+   for ( const wxChar *pc = text.c_str(); ; pc++ )
+   {
+      switch ( *pc )
+      {
+         case _T('*'):
+         case _T('_'):
+            if ( state == Normal )
+            {
+               // output the normal text we had so far
+               m_next->Process(textNormal, viewer, style);
+               textNormal.clear();
+
+               chLastSpecial = *pc;
+               state = chLastSpecial == _T('*') ? Bold : Italic;
+            }
+            else // ending markup tag?
+            {
+               if ( *pc != chLastSpecial )
+               {
+                  // markup mismatch -- consider there was no intention to use
+                  // the last special character for markup at all
+                  textNormal = chLastSpecial + textSpecial + *pc;
+               }
+               else // matching tag
+               {
+                  // output the highlighted text
+                  wxFont fontOld = style.GetFont();
+
+                  wxFont font = fontOld;
+                  if ( chLastSpecial == _T('*') )
+                     font.SetWeight(wxFONTWEIGHT_BOLD);
+                  else // italic
+                     font.SetStyle(wxFONTSTYLE_ITALIC);
+
+                  style.SetFont(font);
+                  m_next->Process(textSpecial, viewer, style);
+                  style.SetFont(fontOld);
+               }
+
+               textSpecial.clear();
+               state = Normal;
+            }
+            break;
+
+         case _T(' '):
+         case _T('\t'):
+         case _T('\r'):
+         case _T('\n'):
+         case _T('\0'):
+            // as a space can't appear inside the marked up word, we decide
+            // that the last special character wasn't mean to begin the markup
+            // after all
+            if ( state != Normal )
+            {
+               textNormal = chLastSpecial + textSpecial;
+               textSpecial.clear();
+               state = Normal;
+            }
+            // fall through
+
+         default:
+            (state == Normal ? textNormal : textSpecial) += *pc;
+      }
+
+      if ( *pc == _T('\0') )
+      {
+         // end of text
+         ASSERT_MSG( state == Normal, _T("logic error in TextMarkupFilter") );
+
+         m_next->Process(textNormal, viewer, style);
+
+         break;
+      }
+   }
+}
+
