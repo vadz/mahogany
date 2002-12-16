9499762ab110650ebc5d07354ef261904e2eccd8
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2002-12-16 01:43:17 +0000

    no real changes

--- src/gui/wxDialogLayout.cpp
+++ src/gui/wxDialogLayout.cpp
@@ -100,7 +100,7 @@ public:
       {
       }
 
-   virtual bool Layout() { return wxPanel::Layout(); }
+   virtual bool Layout() { return wxScrolledWindow::Layout(); }
 };
 
 // ============================================================================
