1a940c34d43395ec7d3e52bff8f7c54b967be358
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2003-03-08 10:15:21 +0000

    updated version to 0.65

--- include/Mversion.h
+++ include/Mversion.h
@@ -19,8 +19,8 @@
 
 /// version info
 #define M_VERSION_MAJOR   0
-#define M_VERSION_MINOR   64
-#define M_VERSION_RELEASE 2
+#define M_VERSION_MINOR   65
+#define M_VERSION_RELEASE 0
 #define M_VERSION_STATUS  "" // "a"=alpha
 
 /// the macros to build the version string from the components
@@ -32,6 +32,6 @@
    M_MAKE_VERSION(M_VERSION_MAJOR, M_VERSION_MINOR, M_VERSION_RELEASE)
 
 /// full version string
-#define M_VERSION_STRING  M_VERSION " 'Sparc'"
+#define M_VERSION_STRING  M_VERSION " 'Claire'"
 
 #endif  //_M_VERSION_H
