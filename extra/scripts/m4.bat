20d5a852d7bd05be6cbdf3a34eac394bfb84fd0c
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2003-09-19 19:26:38 +0000

    don't rely on M4 env var being set

--- extra/scripts/m4.bat
+++ extra/scripts/m4.bat
@@ -6,8 +6,11 @@ rem both on the systems with m4 installed and on the systems without it.
 if .%1==. goto :usage
 if not .%3==. goto :usage
 
-if .%M4%==. goto no_m4
+rem you may edit the line below if m4 is not in your PATH
+set M4=m4
+
 %M4% -DM4FILE=%1\mid2cpp.m4 %1\MInterface.mid > %2\MInterface.cpp
+if errorlevel 1 goto no_m4
 %M4% -DM4FILE=%1\mid2h.m4 %1\MInterface.mid > %2\MInterface.h
 goto end
 
