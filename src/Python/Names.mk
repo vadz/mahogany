3aec3f22dc201dbdf3cdb2b0ffe0599beb28a2eb
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2004-01-06 00:31:24 +0000

    regenerated with swig

--- src/Python/Names.mk
+++ src/Python/Names.mk
@@ -34,21 +34,38 @@ define adjust_dep
 test ! -f $*.d || { sed -e "s,$(@:.o=.cpp),$<," $*.d >$*.d2 && rm -f $*.d && mv $*.d2 $*.d; }
 endef
 
+# and this one replaces #include Python.h in the generated C++ code with
+# #include MPython.h which we need for dynamic Python linking to work
+define create_cpp
+	$(SWIG) -I$(IFACE_DIR) $(CPPFLAGS) $(SWIGFLAGS) \
+	 $(if $(subst swiglib,,$*),-c) -o $*.cpp $< && \
+	sed -e 's/Python\.h/MPython.h/' $*.cpp > $*.cpp.new && mv $*.cpp.new $*.cpp
+endef
+
 SWIGFLAGS := -c++ -python -shadow
 vpath %.i $(IFACE_DIR)
 Python/%.o Python/%.py: %.i
-	cd Python && \
-	$(SWIG) -I../$(IFACE_DIR) $(CPPFLAGS) $(SWIGFLAGS) \
-	 $(if $(subst swiglib,,$*),-c) -o $*.cpp $<
+	cd Python && $(create_cpp)
 	$(M_COMPILE_SWIG)
 	$(adjust_dep)
-else
+
+# define rule for copying the swig-generated files back to the source tree:
+# this is ugly but necessary as we want to store them in the cvs to be able to
+# build M without swig
+.src/Python/%.cpp-swig .src/Python/%.py-swig: %.i
+	$(create_cpp)
+	mv -f $*.cpp .src/Python/$*.cpp-swig
+	cp -f $*.py .src/Python/$*.py-swig
+
+swig-update: $(patsubst %.i,.src/Python/%.cpp-swig,$(IFILES))
+
+else # !SWIG
 vpath %.cpp-swig .src
 %.o: %.cpp-swig
 	cp -f $< $*.cpp
 	cp -f $*.py-swig $*.py
 	$(M_COMPILE_SWIG)
-endif
+endif # SWIG/!SWIG
 
 #CLEAN	+= Python/swiglib.cpp Python/swiglib.py Python/swiglib_wrap.html \
 #	$(POBJS) $(POBJS:.py=.cpp) $(POBJS:.py=_wrap.html)
