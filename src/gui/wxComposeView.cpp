9fe8a685e58ea64024f47a02d3bb087eb82bb8f6
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2004-07-09 10:50:34 +0000

    corrected Shift-Tab and Tab behaviour with wx 2.5.2

--- src/gui/wxComposeView.cpp
+++ src/gui/wxComposeView.cpp
@@ -1235,8 +1235,14 @@ void wxAddressTextCtrl::OnChar(wxKeyEvent& event)
    // we're only interested in TABs and only it's not a second TAB in a row
    if ( event.GetKeyCode() == WXK_TAB )
    {
-      if ( IsModified() &&
-           !event.ControlDown() && !event.ShiftDown() && !event.AltDown() )
+      if ( event.ShiftDown() )
+      {
+#if wxCHECK_VERSION(2, 5, 2)
+         Navigate(0);
+#endif // wx 2.5.2+
+      }
+      else if ( IsModified() &&
+                  !event.ControlDown() && !event.AltDown() )
       {
          // mark control as being "not modified" - if the user presses TAB
          // the second time go to the next window immediately after having
@@ -1255,10 +1261,21 @@ void wxAddressTextCtrl::OnChar(wxKeyEvent& event)
             // don't call event.Skip()
             return;
          }
-         //else: text is empty, treat the TAB normally
+#if wxCHECK_VERSION(2, 5, 2)
+         else
+         {
+            // text is empty, treat the TAB normally
+            Navigate();
+         }
+#endif // wx 2.5.2+
       }
-      //else: nothing because we're not interested in Ctrl-TAB, Shift-TAB &c -
+      //else: nothing because we're not interested in Ctrl-TAB, Alt-TAB &c -
       //      and also in the TABs if the last one was already a TAB
+
+#if wxCHECK_VERSION(2, 5, 2)
+      // skip Skip() below, otherwise a TAB would be inserted in the control
+      return;
+#endif // wx 2.5.2+
    }
 
    // let the text control process it normally: if it's a TAB this will make
