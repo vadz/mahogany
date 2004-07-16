223e621ebf471f495156ba3d17619f57490f7fa3
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2004-07-16 23:53:23 +0000

    no need to reference wxInternetFSHandler from here, it must be explicitly used by HTML viewer

--- src/gui/wxMApp.cpp
+++ src/gui/wxMApp.cpp
@@ -39,7 +39,6 @@
 #include "wx/persctrl.h" // for wxPMessageBoxEnable
 #include <wx/ffile.h>
 #include <wx/fs_mem.h>
-#include <wx/fs_inet.h>
 #include <wx/cmdline.h>
 #include <wx/encconv.h>         // for wxEncodingConverter
 
@@ -992,10 +991,6 @@ wxMApp::OnInit()
    wxInitAllImageHandlers();
    wxFileSystem::AddHandler(new wxMemoryFSHandler);
 //   wxFileSystem::AddHandler(new wxZipFSHandler); // for zipped docs
-
-   // we need to reference wxInternetFSHandler or it might not be linked into
-   // the program at all when linking statically!
-   { wxInternetFSHandler inetfsh; }
 
    // this is necessary to avoid that the app closes automatically when we're
    // run for the first time and show a modal dialog before opening the main
