a87bf296e97e251e037c89cf8efa3b3abcc1e236
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2006-08-23 15:07:13 +0000

    can leave out the 2nd parameter of wxFileType::MessageParameters ctor with recent wx

--- src/gui/wxComposeView.cpp
+++ src/gui/wxComposeView.cpp
@@ -3748,7 +3748,7 @@ bool wxComposeView::StartExternalEditor()
          // we have a handy function in wxFileType which will replace
          // '%s' with the file name or add the file name at the end if
          // there is no '%s'
-         wxFileType::MessageParameters params(tmpFileName.GetName(), wxEmptyString);
+         wxFileType::MessageParameters params(tmpFileName.GetName());
          String command = wxFileType::ExpandCommand(extEdit, params);
 
          // do start the external process
