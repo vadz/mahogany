39bb4b26f15925cc5e5f817604d988de09a9a3ec
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2009-12-05 01:03:17 +0000

    Don't access the terminating NUL by index, this results in an assert from VC9 CRT.

--- src/modules/Filters.cpp
+++ src/modules/Filters.cpp
@@ -302,10 +302,10 @@ public:
    }
 
 protected:
-   void EatWhiteSpace(void)
-      { while(isspace(m_Input[m_Position])) m_Position++; }
    const char Char(void) const
-      { return m_Input[m_Position]; }
+      { return m_Position == m_Input.length() ? '\0' : m_Input[m_Position]; }
+   void EatWhiteSpace(void)
+      { while(isspace(Char())) m_Position++; }
    const char CharInc(void)
       { return m_Input[m_Position++]; }
    String CharLeft(void)
