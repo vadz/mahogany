5cd768ba4f396ac7d87d269bfb526df8b094fccf
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2010-06-27 23:07:57 +0200

    Fix and simplify the Python dialog test example.
    Don't import non-existent since ages MAppBase module. Just use
    MDialogs.Message() to show that it works.

--- src/Python/Scripts/TestDialogs.py
+++ src/Python/Scripts/TestDialogs.py
@@ -1,11 +1,6 @@
-# import all the required M modules, scripts are run in its own namespace
-import Minit, MAppBase, MString
+# import all the required M modules
+import Minit, MDialogs
 
 # show a welcome dialog
 msg = "Welcome, " + Minit.GetUserName() + ", to the wonderful world of M/Python integration!"
-MAppBase.MDialog_Message(msg)
-
-# ask for some input
-result = MString.String("default")
-MAppBase.MInputBox(result,"A simple question...","What do you think?")
-MAppBase.MDialog_Message("You answered: " + result.c_str())
+MDialogs.Message(msg)
