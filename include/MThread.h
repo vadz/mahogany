bb958b6dcfe905254fbabc5f12d311a08b9e090a
Vadim Zeitlin <vz-mahogany@zeitlins.org>
2010-07-08 16:50:03 +0200

    Split MMutex in STMutex and MTMutex, add MutexLocker.
    Instead of defining MMutex class differently depending on whether we build
    with USE_THREADS on or off, split it into two classes: STMutex and MTMutex
    corresponding to the two versions. This allows to gradually start using
    threads in some parts of the program instead of having to enable their use
    everywhere (which would almost certainly break everything).
    
    Also add MutexLocker class which is meant to replace the old MLocker which is
    still kept for now until all of its uses can be examined and replaced with
    either STMutex or MTMutex as appropriate.

--- include/MThread.h
+++ include/MThread.h
@@ -13,66 +13,140 @@
 #ifndef _MTHREAD_H_
 #define _MTHREAD_H_
 
-// ----------------------------------------------------------------------------
-// MMutex: can be in locked or unlocked state, can't be locked more than once
-// ----------------------------------------------------------------------------
+/**
+    @file
+    @brief Various thread-related classes.
 
-#if USE_THREADS
+    For historical reasons we used to have MMutex class which was defined
+    differently depending on whether M was compiled with @c USE_THREADS on or
+    off. As we always use threads now, this doesn't make sense any longer but
+    the existing code using "single thread" version of MMutex can't be blindly
+    changed to work with the real "multi threaded" version. So we currently
+    define both of them and will get rid of the single threaded one in the
+    (distant) future when all code becomes really MT-safe.
+ */
 
-// use inheritance and not typedef to allow forward declaring it
-class MMutex : public wxMutex { };
+#include <wx/thread.h>
 
-#else // !USE_THREADS
+/**
+    Real mutex object.
 
-class MMutex
+    This is a real mutex which prevents more than one thread from acquiring it
+    at any given time. It is non-recursive meaning that it can't locked twice
+    even by a thread that had already locked it before.
+
+    It is implemented simply as wxMutex right now but we could switch to using
+    some other mutex class (e.g. from Boost.Thread) in the future.
+ */
+typedef wxMutex MTMutex;
+
+/**
+    Stand in for a mutex for single-threaded code.
+
+    This class emulates a mutex for single threaded code by just preventing
+    reentrancy, i.e. it makes it impossible for the same thread to lock it
+    twice.
+ */
+class STMutex
 {
 public:
-   MMutex() { m_locked = FALSE; }
+   /// Creates an initially unlocked mutex.
+   STMutex() { m_locked = false; }
 
-   bool IsLocked() const{ return m_locked; }
+   /**
+       Check if the mutex is locked.
 
-   void Lock()
+       This method doesn't exist in the real MTMutex class so it shouldn't be
+       used in the code which is meant to become MT-safe.
+    */
+   bool IsLocked() const { return m_locked; }
+
+   /**
+       Lock the mutex.
+
+       @return wxMUTEX_NO_ERROR or wxMUTEX_DEAD_LOCK.
+    */
+   wxMutexError Lock()
    {
-      ASSERT_MSG( !IsLocked(), _T("attempting to lock locked mutex") );
+      if ( IsLocked() )
+      {
+         FAIL_MSG( "attempting to lock locked mutex" );
+
+         return wxMUTEX_DEAD_LOCK;
+      }
 
       m_locked = true;
+
+      return wxMUTEX_NO_ERROR;
    }
 
-   void Unlock()
+   /**
+       Unlock the mutex.
+
+       @return wxMUTEX_NO_ERROR or wxMUTEX_UNLOCKED.
+    */
+   wxMutexError Unlock()
    {
-      ASSERT_MSG( IsLocked(), _T("attempting to unlock unlocked mutex") );
+      if ( !IsLocked() )
+      {
+         FAIL_MSG( "attempting to unlock unlocked mutex" );
+
+         return wxMUTEX_UNLOCKED;
+      }
 
       m_locked = false;
+
+      return wxMUTEX_NO_ERROR;
    }
 
-   ~MMutex() { ASSERT_MSG( !IsLocked(), _T("deleting locked mutex") ); }
+   /// Dtor checks that the mutex had been unlocked before.
+   ~STMutex() { ASSERT_MSG( !IsLocked(), "deleting locked mutex" ); }
 
 private:
    bool m_locked;
+
+   wxDECLARE_NO_COPY_CLASS(STMutex);
 };
 
-#endif // USE_THREADS/!USE_THREADS
+/**
+    Compatibility name, don't use any longer.
 
-// ----------------------------------------------------------------------------
-// MLocker: lock the mutex in ctor, unlock in dtor
-//
-// NB: unfortunately we already have MMutexLocker elsewhere
-// ----------------------------------------------------------------------------
+    Currently this is just a synonym for STMutex.
+ */
+typedef STMutex MMutex;
 
-class MLocker
+/**
+   Lock the mutex in ctor, unlock in dtor.
+ */
+template <class M>
+class MutexLocker
 {
 public:
-   MLocker(MMutex& mutex) : m_mutex(mutex) { m_mutex.Lock(); }
-   MLocker(MMutex *mutex) : m_mutex(*mutex) { m_mutex.Lock(); }
-   ~MLocker() { m_mutex.Unlock(); }
+   typedef M Mutex;
+
+   MutexLocker(Mutex& mutex) : m_mutex(mutex) { m_mutex.Lock(); }
+   ~MutexLocker() { m_mutex.Unlock(); }
+
+protected:
+   Mutex& m_mutex;
+
+   wxDECLARE_NO_COPY_TEMPLATE_CLASS(MutexLocker, M);
+};
+
+/**
+    Compatibility name, don't use any longer.
+ */
+class MLocker : public MutexLocker<MMutex>
+{
+public:
+   MLocker(MMutex& mutex) : MutexLocker<MMutex>(mutex) { }
+   MLocker(MMutex *mutex) : MutexLocker<MMutex>(*mutex) { }
 
    bool IsLocked() const { return m_mutex.IsLocked(); }
    operator bool() const { return IsLocked(); }
 
 private:
-   MMutex& m_mutex;
-
-   DECLARE_NO_COPY_CLASS(MLocker)
+   wxDECLARE_NO_COPY_CLASS(MLocker);
 };
 
 #endif // _MTHREAD_H_
