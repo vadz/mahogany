vadz/mahogany

commit 87a376647b52ceac5dba1de5d032cd167e5e0d2d

No changes, just move MFCmnOptions implementation inline.
There doesn't seem to be any reason to not have these trivial methods inline.

Also implement operator==() and define operator!=() in terms of it instead of
(unusually) doing vice versa.

include/MailFolderCmn.h
@@ -219,13 +219,18 @@ protected:
    //@{
    struct MFCmnOptions
    {
-      MFCmnOptions();
-
-      bool operator!=(const MFCmnOptions& other) const;
+      MFCmnOptions() { m_UpdateInterval = 0; }
 
       bool operator==(const MFCmnOptions& other) const
       {
-         return !(*this != other);
+         return m_SortParams != other.m_SortParams ||
+                m_ThrParams != other.m_ThrParams ||
+                m_UpdateInterval != other.m_UpdateInterval;
+      }
+
+      bool operator!=(const MFCmnOptions& other) const
+      {
+         return !(*this == other);
       }
 
       /// how to sort the list of messages

src/mail/MailFolderCmn.cpp
@@ -1338,22 +1338,6 @@ MailFolderCmn::RequestUpdate()
 }
 
 // ----------------------------------------------------------------------------
-// MFCmnOptions
-// ----------------------------------------------------------------------------
-
-MailFolderCmn::MFCmnOptions::MFCmnOptions()
-{
-   m_UpdateInterval = 0;
-}
-
-bool MailFolderCmn::MFCmnOptions::operator!=(const MFCmnOptions& other) const
-{
-   return m_SortParams != other.m_SortParams ||
-          m_ThrParams != other.m_ThrParams ||
-          m_UpdateInterval != other.m_UpdateInterval;
-}
-
-// ----------------------------------------------------------------------------
 // MailFolderCmn options handling
 // ----------------------------------------------------------------------------
 

commit b28adfbae9e6cdafa9aad62d6f41377864ed4c35

Fix MFCmnOptions::operator==() broken since 87a37664.
The implementation of operator==() actually used the old code of operator!=()
so comparing MFCmnOptions didn't work correctly, resulting, for example, in
ignoring any attempts to change the sort order.

include/MailFolderCmn.h
@@ -223,9 +223,9 @@ protected:
 
       bool operator==(const MFCmnOptions& other) const
       {
-         return m_SortParams != other.m_SortParams ||
-                m_ThrParams != other.m_ThrParams ||
-                m_UpdateInterval != other.m_UpdateInterval;
+         return m_SortParams == other.m_SortParams &&
+                m_ThrParams == other.m_ThrParams &&
+                m_UpdateInterval == other.m_UpdateInterval;
       }
 
       bool operator!=(const MFCmnOptions& other) const