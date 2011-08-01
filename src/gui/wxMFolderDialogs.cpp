vadz/mahogany

commit 42b2368630f59a2f2f7c88d74f5d9c166a08378b

Do not inherit MF_FLAGS_KEEPOPEN when creating new subfolders.
This flag is typically set only for the few most important folders and
shouldn't be set by default for their children.

src/gui/wxMFolderDialogs.cpp
@@ -2128,7 +2128,10 @@ wxFolderPropertiesPage::SetDefaultValues()
    const int flags = folder->GetFlags();
    m_originalIsHiddenValue = (flags & MF_FLAGS_HIDDEN) != 0;
 
-   m_keepOpen->SetValue((flags & MF_FLAGS_KEEPOPEN) != 0);
+   // do not inherit MF_FLAGS_KEEPOPEN by default, it's typically set only for
+   // a few important folders and shouldn't be propagated to all of their
+   // children
+   m_keepOpen->SetValue(false);
 
 #ifdef USE_LOCAL_CHECKBOX
    m_originalIsLocalValue = (flags & MF_FLAGS_ISLOCAL) != 0;

commit e514c0f9666e0a3a13f2b1aef08e3cfe63c1ddb7

Fix the value of "keep open" checkbox in folder properties dialog.
It was broken by 42b2368 which was meant to only prevent MF_FLAGS_KEEPOPEN
from being enabled by default for new folders but ended up by always clearing
it.

src/gui/wxMFolderDialogs.cpp
@@ -2128,10 +2128,19 @@ wxFolderPropertiesPage::SetDefaultValues()
    const int flags = folder->GetFlags();
    m_originalIsHiddenValue = (flags & MF_FLAGS_HIDDEN) != 0;
 
-   // do not inherit MF_FLAGS_KEEPOPEN by default, it's typically set only for
-   // a few important folders and shouldn't be propagated to all of their
-   // children
-   m_keepOpen->SetValue(false);
+   if ( IsCreating() )
+   {
+      // do not inherit MF_FLAGS_KEEPOPEN by default, it's typically set only
+      // for a few important folders and shouldn't be propagated to all of
+      // their children
+      m_keepOpen->SetValue(false);
+   }
+   else
+   {
+      // do accurately reflect the current value of the flag when viewing
+      // properties of an existing folder
+      m_keepOpen->SetValue((flags & MF_FLAGS_KEEPOPEN) != 0);
+   }
 
 #ifdef USE_LOCAL_CHECKBOX
    m_originalIsLocalValue = (flags & MF_FLAGS_ISLOCAL) != 0;