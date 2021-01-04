vadz/mahogany

commit 5214239b00d8ec5a591f3a06175394ea0efd2cd8

Make wxHTML VFS file names simpler and more unique
Using "this" pointer to ensure uniqueness doesn't really work as it's
perfectly possible to create another HtmlViewer using the same memory
location as a previously existing one, so just use a monotone counter
instead: this is both simpler and more reliable.

And it also doesn't trigger any warnings about pointer truncation in 64
bit MSW builds.

src/modules/HtmlViewer.cpp
@@ -870,10 +870,10 @@ int HtmlViewer::CalculateFontSize(const wxFont& font)
 
 wxString HtmlViewer::GetVirtualFileName(size_t n) const
 {
-   // the image file names must be globally unique, so concatenate the address
-   // of this object together with counter to obtain a really unique name
-   return wxString::Format(_T("Mhtml%08lx%lu.png"),
-                           (unsigned long)this, (unsigned long)n);
+   // the image file names must be globally unique, so use a counter to ensure
+   // we never reuse it
+   static unsigned long s_htmlImage = 0;
+   return wxString::Format(_T("Mhtml%08lx%zd.png"), ++s_htmlImage, n);
 }
 
 wxString HtmlViewer::CreateImageInMemoryFS(const wxImage& image)

commit b0490673f557cbbd7c777f74fb84c3e05f529486

Make wxHTML VFS file names not only simpler, but correct
This fixes a bug introduced in 5214239b0 (Make wxHTML VFS file names
simpler and more unique, 2020-12-07) which never reused a VFS name,
meaning that the same attachment used different names when it was added
and when trying to remove it later, triggering an assert and a memory
leak as the attachment was never actually removed.

src/modules/HtmlViewer.cpp
@@ -193,8 +193,15 @@ private:
    bool m_hasHtmlContents;
 
 
+   // This is used in GetVirtualFileName() to ensure that the names we return
+   // from there are distinct for different objects by incrementing this
+   // counter every time a new viewer object is created.
+   static unsigned long s_viewerCount;
+
    DECLARE_MESSAGE_VIEWER()
 };
+
+unsigned long HtmlViewer::s_viewerCount = 0;
 
 // ----------------------------------------------------------------------------
 // HTML_Handler_META: wxHTML handler for the <meta> tag, see EncodingChanger
@@ -580,6 +587,8 @@ IMPLEMENT_MESSAGE_VIEWER(HtmlViewer,
 
 HtmlViewer::HtmlViewer()
 {
+   s_viewerCount++;
+
    wxFileSystem::AddHandler(new wxInternetFSHandler);
 
    m_window = NULL;
@@ -870,10 +879,7 @@ int HtmlViewer::CalculateFontSize(const wxFont& font)
 
 wxString HtmlViewer::GetVirtualFileName(size_t n) const
 {
-   // the image file names must be globally unique, so use a counter to ensure
-   // we never reuse it
-   static unsigned long s_htmlImage = 0;
-   return wxString::Format(_T("Mhtml%08lx%zd.png"), ++s_htmlImage, n);
+   return wxString::Format(_T("Mhtml%08lx%zd.png"), s_viewerCount, n);
 }
 
 wxString HtmlViewer::CreateImageInMemoryFS(const wxImage& image)