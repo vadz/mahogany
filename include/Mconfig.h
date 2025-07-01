f4070743be8a2440b6de2d4dedf214f4144eac99
Vadim Zeitlin <vadim@zeitlins.org>
2025-07-01 19:56:59 +0200

    Remove unused Str() macro
    This doesn't seem to have been ever used anywhere, so just remove it.

--- include/Mconfig.h
+++ include/Mconfig.h
@@ -162,11 +162,9 @@
 #ifdef USE_STD_STRING
 #   include   <string>
     typedef   std::string String;
-#   define    Str(str)((str).c_str())
 #else
 #   include   <wx/string.h>
     typedef   wxString String;
-#   define    Str(str) str
 #endif
 
 // set the proper STL class names
