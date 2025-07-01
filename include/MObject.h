vadz/mahogany

commit bddbc6dcb76eca43aa9bb52952768fd688d60b3c

Avoid gcc 12 warnings about deprecated default special members
Define copy ctors and assignment operators that we need explicitly, even
by just making them default, to avoid gcc warnings about the use of
deprecated implicitly defined versions.

No real changes.

include/MObject.h
@@ -56,6 +56,13 @@ public:
          m_magic = MOBJECT_MAGIC;
          Register();
       }
+   MObject& operator=(const MObject &oldobj)
+      {
+         MOcheck();
+         oldobj.MOcheck();
+         return *this;
+      }
+
    /** Check validity of this object.
        This function should be called wherever such an object is used,
        especially at the beginning of all methods.
@@ -99,8 +106,12 @@ protected:
    /// a simple magic number as a validity check
    int m_magic;
 #else
+   MObject() = default;
+   MObject(const MObject&) = default;
+   MObject^ operator=(const MObject&) = default;
+
    /// virtual destructor
-   virtual ~MObject() {}
+   virtual ~MObject() = default;
    /// empty MOcheck() method
    void MOcheck(void) const {}
    /// nothing

include/gui/wxllist.h
@@ -363,6 +363,9 @@ struct wxLayoutStyleInfo
                      wxColour *fg = NULL,
                      wxColour *bg = NULL,
                      wxFontEncoding enc = wxFONTENCODING_DEFAULT);
+
+   wxLayoutStyleInfo(const wxLayoutStyleInfo&) = default;
+
    wxLayoutStyleInfo & operator=(const wxLayoutStyleInfo &right);
 
    wxColour & GetBGColour() { return m_bg; }

src/modules/Filters.cpp
@@ -381,6 +381,8 @@ public:
    }
 #endif // DEBUG
 
+   Value& operator=(const Value&) = default;
+
    bool IsValid(void) const
       { MOcheck(); return m_Type != Type_Error; }
    bool IsNumber(void) const

commit 5e177cee3602b7a758baeaa0852f74619f5467e8

Fix typo resulting in error in release build
This was broken back in bddbc6dc (Avoid gcc 12 warnings about deprecated
default special members, 2022-08-20) but went unnoticed.

include/MObject.h
@@ -108,7 +108,7 @@ protected:
 #else
    MObject() = default;
    MObject(const MObject&) = default;
-   MObject^ operator=(const MObject&) = default;
+   MObject& operator=(const MObject&) = default;
 
    /// virtual destructor
    virtual ~MObject() = default;