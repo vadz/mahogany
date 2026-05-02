vadz/mahogany

commit a5da612487f1213167b119d170282d03ff43f443

Get rid of remaining casts to "unsigned long" by using "%zu"
Still no changes, just avoid ugly casts by using the correct formatting
string instead.

src/gui/wxFolderView.cpp
@@ -2862,7 +2862,7 @@ void wxFolderListCtrl::UpdateStatusBar()
 
       String msg;
       if ( m_countSelected )
-         msg.Printf(_("%lu messages selected"), (unsigned long)m_countSelected);
+         msg.Printf(_("%zu messages selected"), m_countSelected);
 
       // determine where should this message go
       wxFrame *frame = GetFrame(this);
@@ -3527,9 +3527,9 @@ void wxFolderView::MoveToNextSearchMatch(bool forward)
          status = String(_T(" (")) + status + String(_T(')'));
       }
 
-      wxLogStatus(m_Frame, _("Search result %lu of %lu for \"%s\"%s"),
-                  (unsigned long)(m_searchData.idx + 1),
-                  (unsigned long)count,
+      wxLogStatus(m_Frame, _("Search result %zu of %zu for \"%s\"%s"),
+                  (m_searchData.idx + 1),
+                  count,
                   m_searchData.str,
                   status);
    }
@@ -5083,8 +5083,8 @@ wxFolderView::OnFolderExpungeEvent(MEventFolderExpungeData& event)
    size_t n,
           count = event.GetCount();
 
-   wxLogTrace(M_TRACE_FV_UPDATE, _T("wxFolderView::Expunge(%lu items), now %d"),
-              (unsigned long)count, m_FolderCtrl->GetItemCount());
+   wxLogTrace(M_TRACE_FV_UPDATE, _T("wxFolderView::Expunge(%zu items), now %d"),
+              count, m_FolderCtrl->GetItemCount());
 
    HeaderInfoList_obj hil(GetFolder()->GetHeaders());
 

src/classes/MModule.cpp
@@ -733,8 +733,7 @@ MModule::ListAvailableModules(const String& interfaceName)
       }
    }
 
-   wxLogTrace(M_TRACE_MODULES, _T("\t%lu modules found."),
-              (unsigned long)count);
+   wxLogTrace(M_TRACE_MODULES, _T("\t%zu modules found."), count);
 
    listing->SetCount(count);
 

src/classes/MObject.cpp
@@ -64,7 +64,7 @@ void MObjectRC::CheckLeaks()
    size_t nCount = gs_aObjects.Count();
 
    if ( nCount > 0 ) {
-      wxLogDebug(_T("MEMORY LEAK: %lu object leaked:"), (unsigned long)nCount);
+      wxLogDebug(_T("MEMORY LEAK: %zu object leaked:"), nCount);
    }
 
    for ( size_t n = 0; n < nCount; n++ ) {
@@ -75,7 +75,7 @@ void MObjectRC::CheckLeaks()
 
 String MObjectRC::DebugDump() const
 {
-   return MObject::DebugDump() + String::Format(_T(" m_nRef = %lu: "), (unsigned long)m_nRef);
+   return MObject::DebugDump() + String::Format(_T(" m_nRef = %zu: "), m_nRef);
 }
 
 void MObject::CheckLeaks()
@@ -112,8 +112,7 @@ void MObjectRC::IncRef()
    if ( this == gs_traceObject )
    {
       wxLogTrace(TRACE_REFCOUNT,
-                 _T("Object %p: IncRef() called, m_nRef = %lu."),
-                 this, (unsigned long)m_nRef);
+                 _T("Object %p: IncRef() called, m_nRef = %zu."), this, m_nRef);
    }
 }
 
@@ -127,8 +126,7 @@ bool MObjectRC::DecRef()
    if ( this == gs_traceObject )
    {
       wxLogTrace(TRACE_REFCOUNT,
-                 _T("Object %p: DecRef() called, m_nRef = %lu."),
-                 this, (unsigned long)m_nRef);
+                 _T("Object %p: DecRef() called, m_nRef = %zu."), this, m_nRef);
    }
 
    if ( m_nRef == 0 )

src/classes/MessageView.cpp
@@ -2140,14 +2140,14 @@ void MessageView::ShowImage(const MimePart *mimepart)
                msg.Printf
                    (
                      _("An image embedded in this message is bigger "
-                       "than the currently configured limit of %luKb.\n"
+                       "than the currently configured limit of %ldKb.\n"
                        "\n"
                        "Would you still like to see it?\n"
                        "\n"
                        "You can change this setting in the \"Message "
                        "View\" page of the preferences dialog to 0 if "
                        "you want to always show the images inline."),
-                     (unsigned long)m_ProfileValues.inlineGFX
+                     m_ProfileValues.inlineGFX
                    );
 
                if ( MDialog_YesNoDialog

src/classes/Profile.cpp
@@ -1098,7 +1098,7 @@ void SaveArray(wxConfigBase *conf,
    size_t nCount = astr.Count();
    String strkey;
    for ( size_t n = 0; n < nCount; n++ ) {
-      strkey.Printf(_T("%lu"), (unsigned long)n);
+      strkey.Printf(_T("%zu"), n);
       conf->Write(path + strkey, astr[n]);
    }
 }
@@ -1113,7 +1113,7 @@ void RestoreArray(wxConfigBase *conf, wxArrayString& astr, const String& key)
 
    String strkey, strVal;
    for ( size_t n = 0; ; n++ ) {
-      strkey.Printf(_T("%lu"), (unsigned long)n);
+      strkey.Printf(_T("%zu"), n);
       if ( !conf->HasEntry(path+strkey) )
          break;
 

src/gui/Mdnd.cpp
@@ -178,8 +178,7 @@ wxDragResult MMessagesDropTarget::OnMsgDrop(wxCoord x, wxCoord y,
    msgCmdProc->ProcessCommand(WXMENU_MSG_DROP_TO_FOLDER, messages, folder);
 
    // it's ok even if m_frame is NULL
-   const unsigned long count = messages.GetCount();
-   wxLogStatus(GetFrame(), _("%lu message(s) dropped."), count);
+   wxLogStatus(GetFrame(), _("%zu message(s) dropped."), messages.GetCount());
    m_where->Refresh();
 
    return def;

src/gui/wxMApp.cpp
@@ -1967,9 +1967,7 @@ wxMApp::UpdateOutboxStatus(MailFolder *mf) const
    if(nNNTP == 0 && nSMTP == 0)
       msg = _("Outbox empty");
    else
-      msg.Printf(_("Outbox %lu, %lu"),
-                 (unsigned long) nSMTP,
-                 (unsigned long) nNNTP);
+      msg.Printf(_("Outbox %lu, %lu"), nSMTP, nNNTP);
 
    wxStatusBar *sbar = m_topLevelFrame->GetStatusBar();
    CHECK_RET( sbar, _T("no status bar in the main frame?") );

src/gui/wxMDialogs.cpp
@@ -2858,8 +2858,7 @@ void MProgressInfo::SetLabel(const wxString& label)
 
 void MProgressInfo::SetValue(size_t numDone)
 {
-   SetLabel(wxString::Format(_("%lu done"),
-                             static_cast<unsigned long>(numDone)));
+   SetLabel(wxString::Format(_("%zu done"), numDone));
 }
 
 MProgressInfo::~MProgressInfo()

commit 2bb5a98f2900548f99b893f07e40f4809c2a48ff

Fix wrong format specifier in wxFolderView::UpdateStatusBar()
This was broken by a5da6124 (Get rid of remaining casts to "unsigned
long" by using "%zu", 2025-07-01), as we need just "%d" here.

src/gui/wxFolderView.cpp
@@ -2862,7 +2862,7 @@ void wxFolderListCtrl::UpdateStatusBar()
 
       String msg;
       if ( m_countSelected )
-         msg.Printf(_("%zu messages selected"), m_countSelected);
+         msg.Printf(_("%d messages selected"), m_countSelected);
 
       // determine where should this message go
       wxFrame *frame = GetFrame(this);